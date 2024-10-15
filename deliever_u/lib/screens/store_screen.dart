90263265f86b2ee07fea95ac33d9bd2c1f2ccff4
FlorezZ1025 <sanaraflorez@gmail.com>
2024-10-14 22:49:50 -0500

    woa

--- deliever_u/lib/screens/store_screen.dart
+++ deliever_u/lib/screens/store_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
-class StoreScreen extends StatelessWidget{
+class StoreScreen extends StatelessWidget {
   StoreScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return const Placeholder();
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Esta es la pantalla de la tienda')),
+        body: const Placeholder());
   }
-
 }
